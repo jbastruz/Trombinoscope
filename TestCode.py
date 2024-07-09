0dd016123b36e7f0c88f673bec5de21bbcbfd0a9
jbastruz <7dfn9kr3@duck.com>
2024-07-09 18:20:55 +0200

    Fix standard picture

--- TestCode.py
+++ TestCode.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import webbrowser
 
-st.set_page_config(layout="wide", page_title="TEST")
+#st.set_page_config(layout="wide", page_title="TEST")
 
 test = pd.read_excel('https://docs.google.com/spreadsheets/d/e/2PACX-1vQ2OZ6qprKlwb8Dq5etxii3n61G-HkxAr-cuuRlqNtyzyn4ThliS2yqTf8cSie3D18Et-lIZeqJ3id6/pub?output=xlsx')
 
@@ -25,16 +25,17 @@ elif Loc == None and Stat != None:
 elif Stat != None and Loc != None:
     filtered = test[(test['Statut'] == Stat) & (test['Lieu'] == Loc)]
 
-col1, col2, col3, col4, col5, col6= st.columns(6)
+col1, col2, col3, col4= st.columns(4)
 
 for i, element in enumerate(filtered['Nom']):
-    col = col1 if i % 6 == 0 else col2 if i % 6 == 1 else col3 if i % 6 == 2 else col4 if i % 6 == 3 else col5 if i % 6 == 4 else col6
+    col = col1 if i % 4 == 0 else col2 if i % 4 == 1 else col3 if i % 4 == 2 else col4
 
     mailto_link= f"mailto:{filtered['Mail'].iloc[i]}"
 
     with col.container(border=True):
         if pd.isna(filtered['Image'].iloc[i])==False: st.image(filtered['Image'].iloc[i]) 
-        else : st.image('https://www.cma-hautesavoie.fr/sites/cma-74/files/cma-haute-savoie-logo_0.png')
+        elif filtered['Image'].iloc[i] == 'Homme' : st.image('https://asadassistance.com/wp-content/uploads/2024/07/avatar-homme.jpg')
+        else : st.image('https://asadassistance.com/wp-content/uploads/2024/07/avatar-femme.jpg')
         st.write(filtered['Nom'].iloc[i]+' '+filtered['Prenom'].iloc[i])
         if pd.isna(filtered['Poste'].iloc[i])==False : st.caption(filtered['Poste'].iloc[i])
         else: st.caption('Poste non renseignée')
